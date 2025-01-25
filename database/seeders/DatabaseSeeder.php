3db583c3f2f70ea5d23c55d1a4f06bac666ae85b
shabihhaider <2022cs723@student.uet.edu.pk>
2025-01-24 19:37:23 -0800

    Designing the URL

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Ticket;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,7 +14,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        $users = \App\Models\User::factory(10)->create();
+
+        // recycle used to assign one ticket from random 100 tickets to a the one user from the 10 users (10 users will get tickets randomly from 100 tickets)
+        Ticket::factory(100)->recycle($users)->create(); 
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Models\Ticket;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,8 +16,25 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// http:localhost:500/api/
+// Universal Resource Locator
+// tickets
+// http:localhost:500/api/tickets/
+// http:localhost:500/api/tickets/{id}
+// http:localhost:500/api/tickets/{id}/edit
+// http:localhost:500/api/tickets/{id}/delete
+// users
+// http:localhost:500/api/users/
+// http:localhost:500/api/users/{id}
+// http:localhost:500/api/users/{id}/edit
+// http:localhost:500/api/users/{id}/delete
+
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
+
+Route::get('/tickets', function() {
+    return Ticket::all();
+});
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
